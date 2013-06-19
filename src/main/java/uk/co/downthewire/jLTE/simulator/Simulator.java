f364a10b7a5568603a3371d89593d94a40ffb597
Andrew Thompson <thompsy@downthewire.co.uk>
2013-06-19 07:51:30 +0100

    Removed some old comments.

--- src/main/java/uk/co/downthewire/jLTE/simulator/Simulator.java
+++ src/main/java/uk/co/downthewire/jLTE/simulator/Simulator.java
@@ -1,6 +1,23 @@
 package uk.co.downthewire.jLTE.simulator;
 
-import flanagan.math.PsRandom;
+import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.ENODEB_CONFIG;
+import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.NUM_UES;
+import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.RBS_PER_SECTOR;
+import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.RESULTS_PATH;
+import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.SCENARIO_PATH;
+import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.UE_CONFIG;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
@@ -16,225 +33,213 @@ import uk.co.downthewire.jLTE.simulator.traffic.TrafficGenerator;
 import uk.co.downthewire.jLTE.simulator.ue.UE;
 import uk.co.downthewire.jLTE.simulator.utils.FieldNames;
 import uk.co.downthewire.jLTE.simulator.x2.X2Router;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
-
-import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.*;
+import flanagan.math.PsRandom;
 
 public class Simulator extends AbstractConfiguredRunnable<SimulationResults> {
 
-    private static final Logger LOG = LoggerFactory.getLogger(Simulator.class);
+	private static final Logger LOG = LoggerFactory.getLogger(Simulator.class);
 
-    public static void main(final String[] args) throws ConfigurationException, InterruptedException, IOException {
-        Configuration configuration = new PropertiesConfiguration("system.properties").interpolatedConfiguration();
-        Simulator sim = new Simulator(configuration);
-        sim.doMain();
-    }
+	public static void main(final String[] args) throws ConfigurationException, InterruptedException, IOException {
+		Configuration configuration = new PropertiesConfiguration("system.properties").interpolatedConfiguration();
+		Simulator sim = new Simulator(configuration);
+		sim.doMain();
+	}
 
-    public Simulator(Configuration configuration) {
-        super(configuration);
-        long seed = (long) configuration.getDouble(FieldNames.SEED);
-        configuration.setProperty(FieldNames.RANDOM_GENERAL, new PsRandom(seed));
-        configuration.setProperty(FieldNames.RANDOM_LOCATION, new PsRandom(seed));
-        configuration.setProperty(FieldNames.RANDOM_SHADOWING, new PsRandom(seed));
-    }
+	public Simulator(Configuration configuration) {
+		super(configuration);
+		long seed = (long) configuration.getDouble(FieldNames.SEED);
+		configuration.setProperty(FieldNames.RANDOM_GENERAL, new PsRandom(seed));
+		configuration.setProperty(FieldNames.RANDOM_LOCATION, new PsRandom(seed));
+		configuration.setProperty(FieldNames.RANDOM_SHADOWING, new PsRandom(seed));
+	}
 
-    public void configureLogging() throws InterruptedException {
-        MDC.put("userid", config.getString(SCENARIO_PATH) + config.getString(RESULTS_PATH) + logName(config));
-        Thread.sleep(5000);
-    }
+	public void configureLogging() throws InterruptedException {
+		MDC.put("userid", config.getString(SCENARIO_PATH) + config.getString(RESULTS_PATH) + logName(config));
+		Thread.sleep(5000);
+	}
 
-    private static void printConfig(Configuration config) {
-        Iterator<String> keyIterator = config.getKeys();
-        while (keyIterator.hasNext()) {
-            String key = keyIterator.next();
-            LOG.error("{} - {}", key, config.getProperty(key));
-        }
-    }
+	private static void printConfig(Configuration config) {
+		Iterator<String> keyIterator = config.getKeys();
+		while (keyIterator.hasNext()) {
+			String key = keyIterator.next();
+			LOG.error("{} - {}", key, config.getProperty(key));
+		}
+	}
 
-    private static String logName(Configuration config) {
-        final SimpleDateFormat formatter = new SimpleDateFormat("hhmmss_ddMMyyyy");
-        String timestamp = formatter.format(new Date());
+	private static String logName(Configuration config) {
+		final SimpleDateFormat formatter = new SimpleDateFormat("hhmmss_ddMMyyyy");
+		String timestamp = formatter.format(new Date());
 
-        String x2String = "";
-        if (config.getBoolean(FieldNames.X2_ENABLED))
-            x2String = "_x2";
+		String x2String = "";
+		if (config.getBoolean(FieldNames.X2_ENABLED))
+			x2String = "_x2";
 
-        return config.getInt(FieldNames.EXPERIMENT_ID) + //
-                "." + config.getInt(FieldNames.CHROMOSOME_ID) + //
-                "_" + config.getString(FieldNames.ALGORITHM) + //
-                x2String + //
-                "_sp" + config.getInt(FieldNames.SPEED) + //
-                "_ues" + config.getInt(FieldNames.NUM_UES) + //
-                "_i" + config.getInt(FieldNames.ITERATIONS) + //
-                "_" + config.getString(FieldNames.TRAFFIC_TYPE) + //
-                "_s" + config.getDouble(FieldNames.SEED) + //
-                "_" + timestamp + ".log";
+		return config.getInt(FieldNames.EXPERIMENT_ID) + //
+		"." + config.getInt(FieldNames.CHROMOSOME_ID) + //
+		"_" + config.getString(FieldNames.ALGORITHM) + //
+		x2String + //
+		"_sp" + config.getInt(FieldNames.SPEED) + //
+		"_ues" + config.getInt(FieldNames.NUM_UES) + //
+		"_i" + config.getInt(FieldNames.ITERATIONS) + //
+		"_" + config.getString(FieldNames.TRAFFIC_TYPE) + //
+		"_s" + config.getDouble(FieldNames.SEED) + //
+		"_" + timestamp + ".log";
 
-    }
+	}
 
-    @Override
-    public String getId() {
-        return config.getInt(FieldNames.EXPERIMENT_ID) + ":" + config.getInt(FieldNames.CHROMOSOME_ID);
-    }
+	@Override
+	public String getId() {
+		return config.getInt(FieldNames.EXPERIMENT_ID) + ":" + config.getInt(FieldNames.CHROMOSOME_ID);
+	}
 
-    @Override
-    public SimulationResults call() throws InterruptedException, IOException {
-        return doMain();
-    }
+	@Override
+	public SimulationResults call() throws InterruptedException, IOException {
+		return doMain();
+	}
 
-    @SuppressWarnings("boxing")
-    public SimulationResults doMain() throws InterruptedException, IOException {
-        Thread.currentThread().setName(String.format("Sim_%d_%d_%d", Thread.currentThread().getId(), config.getInt(FieldNames.EXPERIMENT_ID), config.getInt(FieldNames.CHROMOSOME_ID)));
-        configureLogging();
+	@SuppressWarnings("boxing")
+	public SimulationResults doMain() throws InterruptedException, IOException {
+		Thread.currentThread().setName(String.format("Sim_%d_%d_%d", Thread.currentThread().getId(), config.getInt(FieldNames.EXPERIMENT_ID), config.getInt(FieldNames.CHROMOSOME_ID)));
+		configureLogging();
 
-        final long time = System.currentTimeMillis();
-        LOG.error("Simulator started...");
+		final long time = System.currentTimeMillis();
+		LOG.error("Simulator started...");
 
-        TrafficGenerator trafficGenerator = new TrafficGenerator(config);
-        printConfig(config);
+		TrafficGenerator trafficGenerator = new TrafficGenerator(config);
+		printConfig(config);
 
-        List<ENodeB> enodebs = createENodeBs(config);
-        List<AbstractSector> sectors = getAllSectors(enodebs);
-        List<UE> ues = createUEs(config, enodebs.size(), sectors, trafficGenerator);
+		List<ENodeB> enodebs = createENodeBs(config);
+		List<AbstractSector> sectors = getAllSectors(enodebs);
+		List<UE> ues = createUEs(config, enodebs.size(), sectors, trafficGenerator);
 
-        int numTuples = ues.size() * sectors.size();
-        FadingData fadingData = new FadingData(config, numTuples, config.getInt(RBS_PER_SECTOR));
-        calcServingSectors(ues, sectors, fadingData);
-        logSectors(sectors);
+		int numTuples = ues.size() * sectors.size();
+		FadingData fadingData = new FadingData(config, numTuples, config.getInt(RBS_PER_SECTOR));
+		calcServingSectors(ues, sectors, fadingData);
+		logSectors(sectors);
 
-        X2Router x2Router = new X2Router(config, enodebs);
+		X2Router x2Router = new X2Router(config, enodebs);
 
-        final SimMain main = new SimMain(config, ues, sectors, fadingData, x2Router);
-        main.run();
-        SimulationResults results = new SimulationResults(main.finalStats(ues));
+		final SimMain main = new SimMain(config, ues, sectors, fadingData, x2Router);
+		main.run();
+		SimulationResults results = new SimulationResults(main.finalStats(ues));
 
-        LOG.error("Time taken: {}", Double.toString((System.currentTimeMillis() - time) / 1000.0));
-        return results;
-    }
+		LOG.error("Time taken: {}", Double.toString((System.currentTimeMillis() - time) / 1000.0));
+		return results;
+	}
 
-    private static void logSectors(List<AbstractSector> sectors) {
-        for (AbstractSector sector : sectors) {
-            LOG.debug(sector.toString());
-        }
-    }
+	private static void logSectors(List<AbstractSector> sectors) {
+		for (AbstractSector sector: sectors) {
+			LOG.debug(sector.toString());
+		}
+	}
 
-    private static List<UE> createUEs(Configuration config, int numeNodeBs, List<AbstractSector> sectors, TrafficGenerator trafficGenerator) {
+	private static List<UE> createUEs(Configuration config, int numeNodeBs, List<AbstractSector> sectors, TrafficGenerator trafficGenerator) {
 
-        List<UE> createdUEs = new ArrayList<>();
-        if (config.getInt(NUM_UES) < 1) {
+		List<UE> createdUEs = new ArrayList<>();
+		if (config.getInt(NUM_UES) < 1) {
 
-            // Create the UEs from XML.
-            //TODO: do we ever actually do this?
-            try {
-                final File file = new File(config.getString(SCENARIO_PATH) + config.getString(UE_CONFIG));
-                final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-                final DocumentBuilder db = dbf.newDocumentBuilder();
-                final Document doc = db.parse(file);
-                doc.getDocumentElement().normalize();
-                final NodeList nodes = doc.getElementsByTagName("UE");
+			// Create the UEs from XML.
+			try {
+				final File file = new File(config.getString(SCENARIO_PATH) + config.getString(UE_CONFIG));
+				final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+				final DocumentBuilder db = dbf.newDocumentBuilder();
+				final Document doc = db.parse(file);
+				doc.getDocumentElement().normalize();
+				final NodeList nodes = doc.getElementsByTagName("UE");
 
-                for (int s = 0; s < nodes.getLength(); s++) {
-                    final UE e = UE.fromXML(config, trafficGenerator, nodes.item(s));
-                    createdUEs.add(e);
-                }
-            } catch (final Exception e) {
-                LOG.error("Error creating UEs: {}", e);
-            }
-        } else {
-            // create the UEs randomly
-            for (int i = 0; i < config.getInt(NUM_UES); i++) {
-                final UE e = new UE(config, i, numeNodeBs, sectors, trafficGenerator);
-                createdUEs.add(e);
-            }
+				for (int s = 0; s < nodes.getLength(); s++) {
+					final UE e = UE.fromXML(config, trafficGenerator, nodes.item(s));
+					createdUEs.add(e);
+				}
+			} catch (final Exception e) {
+				LOG.error("Error creating UEs: {}", e);
+			}
+		} else {
+			// create the UEs randomly
+			for (int i = 0; i < config.getInt(NUM_UES); i++) {
+				final UE e = new UE(config, i, numeNodeBs, sectors, trafficGenerator);
+				createdUEs.add(e);
+			}
 
-        }
-        return createdUEs;
-    }
+		}
+		return createdUEs;
+	}
 
-    @SuppressWarnings("boxing")
-    private static List<ENodeB> createENodeBs(Configuration config) {
+	@SuppressWarnings("boxing")
+	private static List<ENodeB> createENodeBs(Configuration config) {
 
-        List<ENodeB> createdeNodeBs = new ArrayList<>();
-        try {
+		List<ENodeB> createdeNodeBs = new ArrayList<>();
+		try {
 
-            final File file = new File(config.getString(SCENARIO_PATH) + config.getString(ENODEB_CONFIG));
-            final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            final DocumentBuilder db = dbf.newDocumentBuilder();
-            final Document doc = db.parse(file);
-            doc.getDocumentElement().normalize();
-            final NodeList nodes = doc.getElementsByTagName("eNodeB");
+			final File file = new File(config.getString(SCENARIO_PATH) + config.getString(ENODEB_CONFIG));
+			final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			final DocumentBuilder db = dbf.newDocumentBuilder();
+			final Document doc = db.parse(file);
+			doc.getDocumentElement().normalize();
+			final NodeList nodes = doc.getElementsByTagName("eNodeB");
 
-            for (int s = 0; s < nodes.getLength(); s++) {
-                final ENodeB e = ENodeB.fromXML(config, nodes.item(s));
-                LOG.info(e.toString());
-                createdeNodeBs.add(e);
-            }
-        } catch (final Exception e) {
-            LOG.error("Error creating eNodeBs: {}", e);
-            throw new IllegalStateException(e);
-        }
+			for (int s = 0; s < nodes.getLength(); s++) {
+				final ENodeB e = ENodeB.fromXML(config, nodes.item(s));
+				LOG.info(e.toString());
+				createdeNodeBs.add(e);
+			}
+		} catch (final Exception e) {
+			LOG.error("Error creating eNodeBs: {}", e);
+			throw new IllegalStateException(e);
+		}
 
-        // Neighbours are determined by their distance.
-        for (ENodeB eNB1 : createdeNodeBs) {
-            for (ENodeB eNB2 : createdeNodeBs) {
-                // Cells are 500m apart, so the immediate neighbours will all be within 600m
-                if (eNB1.id != eNB2.id && Location.calcDistance(eNB1.location, eNB2.location) < 0.6) {
-                    eNB1.addNeighbour(eNB2);
-                }
-            }
-        }
+		// Neighbours are determined by their distance.
+		for (ENodeB eNB1: createdeNodeBs) {
+			for (ENodeB eNB2: createdeNodeBs) {
+				// Cells are 500m apart, so the immediate neighbours will all be within 600m
+				if (eNB1.id != eNB2.id && Location.calcDistance(eNB1.location, eNB2.location) < 0.6) {
+					eNB1.addNeighbour(eNB2);
+				}
+			}
+		}
 
-        for (ENodeB enb : createdeNodeBs) {
-            LOG.debug("eNB[{}]: num neighbours = {}", enb.id, enb.getNeighbours().size());
-        }
+		for (ENodeB enb: createdeNodeBs) {
+			LOG.debug("eNB[{}]: num neighbours = {}", enb.id, enb.getNeighbours().size());
+		}
 
-        return createdeNodeBs;
-    }
+		return createdeNodeBs;
+	}
 
-    private static List<AbstractSector> getAllSectors(List<ENodeB> eNodeBs) {
-        List<AbstractSector> allSectors = new ArrayList<>();
-        for (ENodeB nb : eNodeBs) {
-            allSectors.addAll(nb.getSectors());
-        }
-        return allSectors;
-    }
+	private static List<AbstractSector> getAllSectors(List<ENodeB> eNodeBs) {
+		List<AbstractSector> allSectors = new ArrayList<>();
+		for (ENodeB nb: eNodeBs) {
+			allSectors.addAll(nb.getSectors());
+		}
+		return allSectors;
+	}
 
-    @SuppressWarnings("boxing")
-    public void calcServingSectors(List<UE> ues, List<AbstractSector> sectors, FadingData fadingData) {
+	@SuppressWarnings("boxing")
+	public void calcServingSectors(List<UE> ues, List<AbstractSector> sectors, FadingData fadingData) {
 
-        for (final UE ue : ues) {
+		for (final UE ue: ues) {
 
-            // Now create a list to hold sectors which could serve this UE
-            final List<UESectorTuple> sectorList = new ArrayList<>();
+			// Now create a list to hold sectors which could serve this UE
+			final List<UESectorTuple> sectorList = new ArrayList<>();
 
-            int id = 0;
-            for (final AbstractSector sector : sectors) {
-                final UESectorTuple tuple = new UESectorTuple(config, id++, sector, ue, fadingData);
-                sectorList.add(tuple);
-            }
+			int id = 0;
+			for (final AbstractSector sector: sectors) {
+				final UESectorTuple tuple = new UESectorTuple(config, id++, sector, ue, fadingData);
+				sectorList.add(tuple);
+			}
 
-            java.util.Collections.sort(sectorList);
-            ue.sectorTuples.addAll(sectorList);
-            ue.servingTuple = sectorList.get(sectorList.size() - 1);
+			java.util.Collections.sort(sectorList);
+			ue.sectorTuples.addAll(sectorList);
+			ue.servingTuple = sectorList.get(sectorList.size() - 1);
 
-            final AbstractSector servingSector = sectorList.get(sectorList.size() - 1).sector;
-            servingSector.servedUEs.add(ue);
+			final AbstractSector servingSector = sectorList.get(sectorList.size() - 1).sector;
+			servingSector.servedUEs.add(ue);
 
-            for (UESectorTuple tuple : sectorList) {
-                LOG.info(tuple.toString());
-            }
+			for (UESectorTuple tuple: sectorList) {
+				LOG.info(tuple.toString());
+			}
 
-            LOG.info("UE[{}] served by Sector[{}:{}]", ue.id, servingSector.servingENodeBId, servingSector.id);
-        }
-    }
+			LOG.info("UE[{}] served by Sector[{}:{}]", ue.id, servingSector.servingENodeBId, servingSector.id);
+		}
+	}
 
 }
--- src/main/java/uk/co/downthewire/jLTE/simulator/traffic/TrafficGenerator.java
+++ src/main/java/uk/co/downthewire/jLTE/simulator/traffic/TrafficGenerator.java
@@ -1,91 +1,106 @@
 package uk.co.downthewire.jLTE.simulator.traffic;
 
-import flanagan.math.PsRandom;
+import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.HEAVY;
+import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.LIGHT;
+import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.MIXED;
+import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.TRAFFIC_FULL_PROBABILITY;
+import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.TRAFFIC_HEAVY_PROBABILITY;
+import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.TRAFFIC_HEAVY_PROPORTION_LIGHT;
+import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.TRAFFIC_HEAVY_PROPORTION_MIXED;
+import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.TRAFFIC_HEAVY_RBS;
+import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.TRAFFIC_LIGHT_PROBABILITY;
+import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.TRAFFIC_LIGHT_PROPORTION_LIGHT;
+import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.TRAFFIC_LIGHT_PROPORTION_MIXED;
+import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.TRAFFIC_LIGHT_RBS;
+import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.TRAFFIC_MIXED_PROBABILITY;
+import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.TRAFFIC_MIXED_PROPORTION_LIGHT;
+import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.TRAFFIC_MIXED_PROPORTION_MIXED;
+import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.TRAFFIC_MIXED_RBS;
+import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.TRAFFIC_TYPE;
+
 import org.apache.commons.configuration.Configuration;
 
 import uk.co.downthewire.jLTE.simulator.utils.FieldNames;
-
-import static uk.co.downthewire.jLTE.simulator.utils.FieldNames.*;
+import flanagan.math.PsRandom;
 
 public class TrafficGenerator {
 
-    private final Configuration configuration;
+	private final Configuration configuration;
 
-    public TrafficGenerator(Configuration configuration) {
-        this.configuration = configuration;
-    }
+	public TrafficGenerator(Configuration configuration) {
+		this.configuration = configuration;
+	}
 
-    public double getDownlinkProb(TrafficType trafficType) {
-        if (trafficType == TrafficType.LIGHT) {
-            return configuration.getDouble(TRAFFIC_LIGHT_PROBABILITY);
-        } else if (trafficType == TrafficType.MIXED) {
-            return configuration.getDouble(TRAFFIC_MIXED_PROBABILITY);
-        } else if (trafficType == TrafficType.HEAVY) {
-            return configuration.getDouble(TRAFFIC_HEAVY_PROBABILITY);
-        } else if (trafficType == TrafficType.FULL) {
-            return configuration.getDouble(TRAFFIC_FULL_PROBABILITY);
-        }
+	public double getDownlinkProb(TrafficType trafficType) {
+		if (trafficType == TrafficType.LIGHT) {
+			return configuration.getDouble(TRAFFIC_LIGHT_PROBABILITY);
+		} else if (trafficType == TrafficType.MIXED) {
+			return configuration.getDouble(TRAFFIC_MIXED_PROBABILITY);
+		} else if (trafficType == TrafficType.HEAVY) {
+			return configuration.getDouble(TRAFFIC_HEAVY_PROBABILITY);
+		} else if (trafficType == TrafficType.FULL) {
+			return configuration.getDouble(TRAFFIC_FULL_PROBABILITY);
+		}
 
-        throw new IllegalArgumentException("Unknown traffic type.");
-    }
+		throw new IllegalArgumentException("Unknown traffic type.");
+	}
 
-    public int getRBsToSend(TrafficType trafficType) {
-        if (trafficType == TrafficType.LIGHT) {
-            return configuration.getInt(TRAFFIC_LIGHT_RBS);
-        } else if (trafficType == TrafficType.MIXED) {
-            return configuration.getInt(TRAFFIC_MIXED_RBS);
-        } else {
-            return configuration.getInt(TRAFFIC_HEAVY_RBS);
-        }
-    }
+	public int getRBsToSend(TrafficType trafficType) {
+		if (trafficType == TrafficType.LIGHT) {
+			return configuration.getInt(TRAFFIC_LIGHT_RBS);
+		} else if (trafficType == TrafficType.MIXED) {
+			return configuration.getInt(TRAFFIC_MIXED_RBS);
+		} else {
+			return configuration.getInt(TRAFFIC_HEAVY_RBS);
+		}
+	}
 
-    // TODO: this is still pretty nasty, but at least is a bit more tidy.
-    public TrafficType getTrafficType(double rr) {
+	public TrafficType getTrafficType(double rr) {
 
-        if (configuration.getString(TRAFFIC_TYPE).equalsIgnoreCase(LIGHT)) {
-            if (rr <= configuration.getDouble(TRAFFIC_LIGHT_PROPORTION_LIGHT)) {
-                return TrafficType.LIGHT;
-            } else if (rr <= configuration.getDouble(TRAFFIC_LIGHT_PROPORTION_LIGHT) + configuration.getDouble(TRAFFIC_LIGHT_PROPORTION_MIXED)) {
-                return TrafficType.MIXED;
-            } else {
-                return TrafficType.HEAVY;
-            }
+		if (configuration.getString(TRAFFIC_TYPE).equalsIgnoreCase(LIGHT)) {
+			if (rr <= configuration.getDouble(TRAFFIC_LIGHT_PROPORTION_LIGHT)) {
+				return TrafficType.LIGHT;
+			} else if (rr <= configuration.getDouble(TRAFFIC_LIGHT_PROPORTION_LIGHT) + configuration.getDouble(TRAFFIC_LIGHT_PROPORTION_MIXED)) {
+				return TrafficType.MIXED;
+			} else {
+				return TrafficType.HEAVY;
+			}
 
-        } else if (configuration.getString(TRAFFIC_TYPE).equalsIgnoreCase(MIXED)) {
+		} else if (configuration.getString(TRAFFIC_TYPE).equalsIgnoreCase(MIXED)) {
 
-            if (rr <= configuration.getDouble(TRAFFIC_MIXED_PROPORTION_LIGHT)) {
-                return TrafficType.LIGHT;
-            } else if (rr <= configuration.getDouble(TRAFFIC_MIXED_PROPORTION_LIGHT) + configuration.getDouble(TRAFFIC_MIXED_PROPORTION_MIXED)) {
-                return TrafficType.MIXED;
-            } else {
-                return TrafficType.HEAVY;
-            }
+			if (rr <= configuration.getDouble(TRAFFIC_MIXED_PROPORTION_LIGHT)) {
+				return TrafficType.LIGHT;
+			} else if (rr <= configuration.getDouble(TRAFFIC_MIXED_PROPORTION_LIGHT) + configuration.getDouble(TRAFFIC_MIXED_PROPORTION_MIXED)) {
+				return TrafficType.MIXED;
+			} else {
+				return TrafficType.HEAVY;
+			}
 
-        } else if (configuration.getString(TRAFFIC_TYPE).equalsIgnoreCase(HEAVY)) {
+		} else if (configuration.getString(TRAFFIC_TYPE).equalsIgnoreCase(HEAVY)) {
 
-            if (rr <= configuration.getDouble(TRAFFIC_HEAVY_PROPORTION_LIGHT)) {
-                return TrafficType.LIGHT;
-            } else if (rr <= configuration.getDouble(TRAFFIC_HEAVY_PROPORTION_LIGHT) + configuration.getDouble(TRAFFIC_HEAVY_PROPORTION_MIXED)) {
-                return TrafficType.MIXED;
-            } else {
-                return TrafficType.HEAVY;
-            }
-        } else if (configuration.getString(TRAFFIC_TYPE).equalsIgnoreCase(FieldNames.FULL)) {
-            return TrafficType.FULL;
-        } else {
-            throw new IllegalArgumentException("Invalid traffic type");
-        }
-    }
+			if (rr <= configuration.getDouble(TRAFFIC_HEAVY_PROPORTION_LIGHT)) {
+				return TrafficType.LIGHT;
+			} else if (rr <= configuration.getDouble(TRAFFIC_HEAVY_PROPORTION_LIGHT) + configuration.getDouble(TRAFFIC_HEAVY_PROPORTION_MIXED)) {
+				return TrafficType.MIXED;
+			} else {
+				return TrafficType.HEAVY;
+			}
+		} else if (configuration.getString(TRAFFIC_TYPE).equalsIgnoreCase(FieldNames.FULL)) {
+			return TrafficType.FULL;
+		} else {
+			throw new IllegalArgumentException("Invalid traffic type");
+		}
+	}
 
-    public int generateTraffic(TrafficType trafficType, double random) {
-        PsRandom generalRandom = (PsRandom) configuration.getProperty(FieldNames.RANDOM_GENERAL);
-        if (trafficType == TrafficType.FULL) {
-            return getRBsToSend(trafficType);
-        }
-        int numRBsToGenerate = 0;
-        if (random > 1 - getDownlinkProb(trafficType)) {
-            numRBsToGenerate = generalRandom.nextInteger(getRBsToSend(trafficType)) + 1;
-        }
-        return numRBsToGenerate;
-    }
+	public int generateTraffic(TrafficType trafficType, double random) {
+		PsRandom generalRandom = (PsRandom) configuration.getProperty(FieldNames.RANDOM_GENERAL);
+		if (trafficType == TrafficType.FULL) {
+			return getRBsToSend(trafficType);
+		}
+		int numRBsToGenerate = 0;
+		if (random > 1 - getDownlinkProb(trafficType)) {
+			numRBsToGenerate = generalRandom.nextInteger(getRBsToSend(trafficType)) + 1;
+		}
+		return numRBsToGenerate;
+	}
 }
